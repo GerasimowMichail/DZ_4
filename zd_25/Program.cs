d64c924e3571d4fdf6f275d1885264750b64b5ef
Mischa <GerasimowMichail@yandex.ru>
2022-11-04 22:09:33 +0300

    Начало выполнения ДЗ

---
+++ zd_25/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два 
+// 7числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+Console.WriteLine("Введите Число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Число B: ");
+
+int B = Convert.ToInt32(Console.ReadLine());
+
+int arg1=A;
+int arg2=B;
+int arg=1;
+int i=0;
+// int Degree(int arg1, int arg2)
+// {
+    while (i<=arg2)
+    {
+    arg = arg * arg1;
+    i++;
+    }
+
+    Console.WriteLine(arg1);
+    // return arg1;
+// }
+
+// int res = Degree(A, B);
+// Console.WriteLine(res);
