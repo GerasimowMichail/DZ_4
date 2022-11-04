1ffd9df989e4078681ae3832cdf8cf2daf6dd3eb
Mischa <GerasimowMichail@yandex.ru>
2022-11-05 00:17:58 +0300

    Решение задачи 27

--- zd_27/Program.cs
+++ zd_27/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов с клавиатуры и выводит массив на экран.
+Console.WriteLine("Введите 8 чисел");
+int [] arr=new int[8];
+for (int i=0; i<8; i++ )
+{
+    arr[i]=Convert.ToInt32(Console.ReadLine());
+     
+}
+Console.Write("Введенный массив: ");
+for (int i=0; i<8; i++)
+{
+      Console.Write($"{arr[i]}"+" ");
+}
