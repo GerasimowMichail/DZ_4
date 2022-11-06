28537c6b670dda69f7a9fcc9e23cfc857a1410bf
Mischa <GerasimowMichail@yandex.ru>
2022-11-06 19:24:35 +0300

    Обязательные задачи с функциями

--- zd_25/Program.cs
+++ zd_25/Program.cs
@@ -2,11 +2,12 @@
 // 7числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
 Console.Write("Введите Число А: ");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите Число B: ");
-
 int B = Convert.ToInt32(Console.ReadLine());
+
 
 int arg = 1;
 int i = 0;
--- zd_27/Program.cs
+++ zd_27/Program.cs
@@ -1,14 +1,26 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов
 // с клавиатуры и выводит массив на экран.
-Console.WriteLine("Введите 8 чисел");
+
+int [] Read(string message)
+{
+Console.WriteLine(message);
 int [] arr=new int[8];
 for (int i=0; i<8; i++ )
 {
-    arr[i]=Convert.ToInt32(Console.ReadLine());
-     
+    arr[i]=Convert.ToInt32(Console.ReadLine());     
 }
+return arr;
+}
+int [] mes=Read("Введите 8 элементов");
+
+
+int [] Print(int [] mes)
+{
 Console.Write("Введенный массив: ");
 for (int i=0; i<8; i++)
 {
-      Console.Write($"{arr[i]}"+" ");
+      Console.Write($"{mes[i]}"+" ");
 }
+return mes;
+}
+Print(mes);
